d37c221974c1635c13eed96fdb8a477b7002c2ec
Mohsen <baqeri.me@gmail.com>
2025-04-27 02:22:33 +0330

    Data Types Added !

---
+++ src/Data_Types.kt
@@ -0,0 +1,43 @@
+fun main() {
+
+    // Kotlin is Type Inference Language !
+
+    // Kotlin Data Types !
+
+    // Boolean or bool
+    var flag: Boolean = false
+    var flag2: Boolean = true
+
+    // String
+    var name: String = "Mohsen"
+
+    // Character
+    var c: Char = 'A'
+
+    // Numbers ! 🙂
+
+    // Byte from -128 until 127
+    var number1: Byte = 10
+
+    // Short from -32768 until 32767
+    var number2: Short = 20000
+
+    // Int
+    var number3: Int = 1000000000
+
+    // Long
+    var number4: Long = 100000000000000
+
+    // Float
+    // we use F at the end of number to define it as float !
+    var number5: Float = 1.2F
+
+    // Double
+    var number6: Double = 1.264523154687
+
+
+    // we can define a number with _ to make it easy to read in Kotlin !
+    var number7 = 1_000_000_000
+
+
+}
