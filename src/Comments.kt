88a0b0b85c38b307e5337186ee6295cb4b3799dd
Mohsen <baqeri.me@gmail.com>
2025-04-27 01:44:21 +0330

    comments added !

---
+++ src/Comments.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    // this is a single line comment in Kotlin
+
+    /*
+    this is a Multi-line comment !
+    second line !
+     */
+
+    /**
+     * if we want to make a document
+     * we can use this Syntax !
+     */
+
+
+}
