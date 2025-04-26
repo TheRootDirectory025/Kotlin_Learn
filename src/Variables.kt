f1d5765923525a96d23849d6154b65e697981dc1
Mohsen <baqeri.me@gmail.com>
2025-04-27 01:36:30 +0330

    variables added !

---
+++ src/Variables.kt
@@ -0,0 +1,24 @@
+fun main(){
+
+    // if we want to Define an Constant We use (val) key
+    val ID_number = 123456789
+    val name = "Mohsen"
+
+    // we can also define a val with its type
+    val family_name : String = "Bagheri"
+
+    // we can not change a constant value !
+//    family_name = "mohammadi"
+
+
+
+    // in Kotlin we can define a variable with (var)
+    var age = 20
+    age = 21
+
+    // this is error of type safe Variables in Kotlin
+//    age = "ali"
+
+
+
+}
