b119fe05f539081772997cce66b6f973412280ed
Mohsen <baqeri.me@gmail.com>
2025-04-29 19:02:43 +0330

    String Interpulate Added !

---
+++ src/String_Interpulation.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var iPhone16ProMax: Int = 150_000_000
+
+    // only we can add a variable with + after String !
+    println("String" + iPhone16ProMax)
+
+    // we can interpulate a variable into string with $
+    println("$iPhone16ProMax String")
+}
