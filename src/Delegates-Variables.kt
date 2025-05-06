dade9dad94f56a9cdba94dd6a69c9f0434de53d1
Mohsen <baqeri.me@gmail.com>
2025-05-06 04:03:32 +0330

    Delegates-Variables !

---
+++ src/Delegates-Variables.kt
@@ -0,0 +1,11 @@
+import kotlin.properties.Delegates
+
+fun main(){
+    // here are some ways to make a variable
+    var num : Int
+    var num2 : Int? = null
+
+    // Delegates will make this variable if we want to use it . Not now !!!!
+    // this Variable will never accept null !
+    var num3 by Delegates.notNull<Int>()
+}
