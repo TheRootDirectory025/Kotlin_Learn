ae3d28e7a99a6c38c98f256289c520e0e56bdbb2
Mohsen <baqeri.me@gmail.com>
2025-05-06 03:54:50 +0330

    about back-slash !

---
+++ src/Back_Slash.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val name = "Mohsen Bagheri."
+    // \b is Back-Space
+    println("$name\b")
+    // \n moves to new line
+    println("$name\nnew line")
+    // \t print a TAB
+    println("$name\t test text")
+    // printing \
+    println("$name \\ end")
+    // printing " or '
+    println("$name \" and \' end")
+
+}
