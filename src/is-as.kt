84e1cf5e13ffe0093bd80ae1b5e362674360b5b5
Mohsen <baqeri.me@gmail.com>
2025-05-23 19:57:04 +0330

    is-as added !

---
+++ src/is-as.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Any-thing !
+    var variable: Any = 125
+
+    // type-casting with as
+    var num = variable as Int
+
+
+    // is is a if cluse
+    var flag = num is Int // true
+
+
+}
