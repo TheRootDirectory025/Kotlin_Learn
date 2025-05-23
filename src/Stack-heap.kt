a31fbeafd6effc41e2b12f1e7a291dfa87f9be94
Mohsen <baqeri.me@gmail.com>
2025-05-23 18:27:44 +0330

    Ram discription added !

--- src/Stack-heap.kt
+++ src/Stack-heap.kt
@@ -15,8 +15,22 @@ fun main(){
     val number : Int = 5
     val number2 : Float = 5.1f
 
+
+
+
     // Heap
     // none-primitive Datas
+
+    // String Constant Pool
+    /***
+     * in Heap We have String constant pool
+     * String Constant pool stores string variables with same value just like python
+     * with same Storage and just a pointer !
+     */
     val name : String = "Kotlin"
+    val name2 : String = "Kotlin"
+    println(System.identityHashCode(name))
+    println(System.identityHashCode(name2))
+
 
 }
